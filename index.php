6184d11878dda75ad1bc19a0f4a9a8150e7ad333
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-21 16:02:17 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <h1>Routing</h1>
+
+  <p>
+    Dans une application moderne qui suit le MVC(Model-Vue-Controller), il est nécessaire de mettre en place coté "back-end" un système qui permet un navigation cohérente entre les pages. Ce système s'applle "le Routing".
+
+    Le routing ou "routage " , permet de définir comment les "URL" d'une application doivent etre mappées.
+    Quant au mapping des URL , il s'agit du processus consistant à associer une URL à une action spécifique
+  En effet, à chaque requêteHTTP, l'application doit savoir non seulement quelle route est active mais aussi quelle méthode de controleur est associée.
+    Une route peut également contenir des paramètres qui permettent aux controlleursde fournir des informations pécises. Ainsi, le routing est important pour améliorer l'experience utilisateur, car il permet de fournir des "URL" claires et indexées par les moteurs de recherches.
+
+    Le fraùwork Sumfony fournit bien evidement les outils adaptés pour maitriser le routing. Mais comment fonctionne-t-il? Quelles sont les erreurs à éviter?
+    C'est ce que nous aborderons à travers ce cours sur le routing dans Symfony 5.4 .
+  </p>
+
+ 
+  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+  </body>
+</html>
